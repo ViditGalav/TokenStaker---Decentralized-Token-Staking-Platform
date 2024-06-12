d62752b1e47e2345ddab81e5b802f39cab9ae08d
unknown <viditgalav25@gmail.com>
2024-06-13 02:09:33 +0530

    Token Staking is  working and Tested Successfully

---
+++ client/src/components/DisplayPannel/DisplayPannel.jsx
@@ -0,0 +1,19 @@
+import RewardRate from "./RewardRate";
+import StakedAmount from "./StakedAmount";
+import EarnedReward from "./EarnedReward";
+
+const DisplayPannel = ()=>{
+  return(   
+  <div className="top-wrapper">
+    <StakedAmount/>
+    <RewardRate/>
+    <EarnedReward/>
+  </div>
+  )
+}
+export default DisplayPannel;
+
+
+
+
+
